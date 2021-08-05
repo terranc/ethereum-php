2e15c1cecbf4ccce010e72ca7d679ca8a88ae72d
TerranChao <wuchaoboy@gmail.com>
2021-08-05 22:25:18 +0800

    update

---
+++ src/RPC/AlchemyAPI.php
@@ -0,0 +1,63 @@
+<?php
+/*
+ * This file is a part of "furqansiddiqui/ethereum-php" package.
+ * https://github.com/furqansiddiqui/ethereum-php
+ *
+ * Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/furqansiddiqui/ethereum-php/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace FurqanSiddiqui\Ethereum\RPC;
+
+use FurqanSiddiqui\Ethereum\Ethereum;
+use FurqanSiddiqui\Ethereum\Exception\RPCInvalidResponseException;
+use FurqanSiddiqui\Ethereum\RPC\Models\Transfer;
+
+/**
+ * Class InfuraAPI
+ * @package FurqanSiddiqui\Ethereum\RPC
+ */
+class AlchemyAPI extends AbstractRPCClient
+{
+    /** @var string */
+    private string $key;
+
+    /**
+     * InfuraAPI constructor.
+     * @param Ethereum $eth
+     * @param string $key
+     */
+    public function __construct(Ethereum $eth, string $key)
+    {
+        parent::__construct($eth);
+
+        $this->key = $key;
+
+
+        // Set HTTP auth basic
+//        $this->httpAuthBasic("", $this->projectSecret);
+    }
+
+    /**
+     * @return string
+     */
+    protected function getServerURL(): string
+    {
+        return sprintf('https://eth-mainnet.alchemyapi.io/v2/', $this->key);
+    }
+
+    public function alchemy_getAssetTransfers(array $param)
+    {
+        $transfers = $this->call("alchemy_getAssetTransfers", [$param]);
+        if (!is_array($transfers)) {
+            throw RPCInvalidResponseException::InvalidDataType("eth_sendRawTransaction", "Object", gettype($transfers));
+        }
+
+        return new Transfer($this->eth, $transfers);
+    }
+}
